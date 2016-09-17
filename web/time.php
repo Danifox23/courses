d92fa23f3e2729cb77451e828a6d92f337de96e4
Даниил Сидоров < >
2016-09-17 15:18:37 +0500

    First commit

---
+++ web/time.php
@@ -0,0 +1,15 @@
+<?php
+$date_today = date("m.d.y");
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Заголовок страницы</title>
+</head>
+<body>
+<div class="container">
+    <h1><?php echo $date_today; ?></h1>
+</div>
+</body>
+</html>
